be8a8da748703273cbe928b25456d9dd1dcc9976
Pijay <53463564+yopijay@users.noreply.github.com>
2025-08-17 18:24:44 +0800

    feat: global files (fetch, store, types)

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,8 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const fetchAPI = async (url: string, options?: RequestInit) => {
     try {
         const response = await fetch(url, options);
+
         if (!response.ok) {
             new Error(`HTTP error! status: ${response.status}`);
         }
--- types/type.d.ts
+++ types/type.d.ts
@@ -36,15 +36,14 @@ declare interface MapProps {
 declare interface Ride {
     origin_address: string;
     destination_address: string;
-    origin_latitude: number;
-    origin_longitude: number;
-    destination_latitude: number;
-    destination_longitude: number;
+    origin_latitude: string;
+    origin_longitude: string;
+    destination_latitude: string;
+    destination_longitude: string;
     ride_time: number;
-    fare_price: number;
+    fare_price: string;
     payment_status: string;
     driver_id: number;
-    user_email: string;
     created_at: string;
     driver: {
         first_name: string;
